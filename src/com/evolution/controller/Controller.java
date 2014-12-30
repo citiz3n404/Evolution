714ef4fba2fe1826313b649ee08161054968e548
NightWolf <anthony92@live.fr>
2014-12-30 23:15:17 +0100

    Petite correction sur la sauvegarde/chargement

--- src/com/evolution/controller/Controller.java
+++ src/com/evolution/controller/Controller.java
@@ -12,10 +12,10 @@ import javax.swing.JTextField;
  * @author Anthony
  */
 public class Controller implements CONSTANTS, Serializable {
+
     public Model m;
     private int[] tempTab;
 
-    
     public Controller(Model mParam) {
         m = mParam;
         tempTab = new int[10];
@@ -25,8 +25,8 @@ public class Controller implements CONSTANTS, Serializable {
     public void endWindow() {
         System.exit(0);
     }
-    
-    public void saveWorld(){
+
+    public void saveWorld() {
         String txt = new String();
         JFileChooser chooser = new JFileChooser();
         chooser.setApproveButtonText("Enregistrer");
@@ -36,30 +36,31 @@ public class Controller implements CONSTANTS, Serializable {
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             File selection = chooser.getSelectedFile();
             txt = selection.getPath();
+            System.out.println(txt);
+            m.saveModel(txt);
         }
+
         
-        System.out.println(txt);
-        m.saveModel(txt);
     }
-    
-    public void loadWorld(){
-        
+
+    public void loadWorld() {
+
         String txt = new String();
         JFileChooser chooser = new JFileChooser();
         int returnVal = chooser.showOpenDialog(null);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             File selection = chooser.getSelectedFile();
             txt = selection.getPath();
+            m.resetModel();
+            System.out.println(txt);
+
+            setValues();
+            m.validWorld = true;
+
+            m.loadModel(txt);
+            m.notifyObserver();
         }
-        
-        System.out.println(txt);
-        m.resetModel();
-        
-        setValues();
-        m.validWorld = true;
-        
-        m.loadModel(txt);
-        m.notifyObserver();
+
     }
 
     public void initOptimal() {
@@ -68,7 +69,6 @@ public class Controller implements CONSTANTS, Serializable {
          0 -> SIZEX  1 -> SIZEY  2 -> WOLF  3 -> SHEEP  4 -> GRASS
          5 -> MINERALS  6 -> NB CASES  7 -> NB ANIMALS 8 -> NB GRASS + MINERALS
          */
-        
         tempTab[0] = NB_COLUMN_OPTIMAL;
         tempTab[1] = NB_LINE_OPTIMAL;
         tempTab[2] = NB_WOLFS_OPTIMAL;
@@ -120,28 +120,26 @@ public class Controller implements CONSTANTS, Serializable {
 
     public void setTempTab(int index, int v) {
         this.tempTab[index] = v;
-        
-        if(index == 0 || index == 1){
-            this.tempTab[6] = tempTab[0]*tempTab[1];
-        }
-        else if(index == 2 || index == 3){
+
+        if (index == 0 || index == 1) {
+            this.tempTab[6] = tempTab[0] * tempTab[1];
+        } else if (index == 2 || index == 3) {
             this.tempTab[7] = tempTab[2] + tempTab[3];
-        }
-        else if(index == 4 || index == 5){
+        } else if (index == 4 || index == 5) {
             this.tempTab[8] = tempTab[4] + tempTab[5];
         }
         m.notifyObserver();
 
     }
-    
-    public void initSimulation(){
+
+    public void initSimulation() {
         m.initUniverse(); // Crée les cases
         m.initAnimals(); // Crée les animaux
         m.initElements(); // herbe et mineraux
         m.notifyObserver(); // Actualise
-       
+
         m.playSimulation();
-                
+
     }
 
 }
